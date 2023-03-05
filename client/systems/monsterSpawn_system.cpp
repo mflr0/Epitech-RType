20b62528846a9bbfae85fb7a6dd7dedad4b1f6ee
mflr0 <mflr0@onionmail.org>
2023-03-05 19:26:58 +0100

    Updated monsterSpawn_system, now multiple enemies mutually spawn

--- client/systems/monsterSpawn_system.cpp
+++ client/systems/monsterSpawn_system.cpp
@@ -5,18 +5,21 @@
 #include "client/rtype.h"
 
 void monsterSpawnSystem::update(sf::RenderWindow &window, World &world, float delta) {
-    _spawnRate = 100;
+    _maxSpawn = rand() % 10 + 1;
+    _spawnRate = rand() % 100 + 200;
     _spawnPositionY = rand() % (window.getSize().y - 100) + 100;
-    _spawnPositionX = window.getSize().x + rand() % 100;
-    _monsterSpeedX = rand() % 100 + 100;
-    _monsterSpeedY = rand() % 20 + 20;
+    _spawnPositionX = window.getSize().x + rand() % 100 + 120;
+    _monsterSpeedX = rand() % 100 + 50;
 
 
-    if (_spawnTimer <= 0) {
+    if (_spawnCount < _maxSpawn) {
         _monsterType = rand() % 4;
-        monsterType type = static_cast<monsterType>(_monsterType);
+        auto type = static_cast<monsterType>(_monsterType);
 
         std::shared_ptr<Monster> monster = world.createEntity<Monster>(type);
+        _monsterSpeedY = rand() % 50;
+        if (rand() % 2 == 0)
+            _monsterSpeedY = -_monsterSpeedY;
         monster->getComponent<PositionComponent>()->setPosition(_spawnPositionX, _spawnPositionY);
         monster->getComponent<HealthComponent>()->setHealth(100);
         monster->getComponent<HealthComponent>()->setDamage(100);
@@ -24,6 +27,8 @@ void monsterSpawnSystem::update(sf::RenderWindow &window, World &world, float de
 
         _spawnCount++;
         _spawnTimer = _spawnRate;
+    } else if (_spawnTimer <= 0) {
+        _spawnCount = 0;
     } else {
         _spawnTimer--;
     }
